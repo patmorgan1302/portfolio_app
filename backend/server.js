d525fcbe143494e7e037346b9e1cf792bc106747
patmorgan1302 <patrickmorgan1302@gmail.com>
2024-10-02 16:21:50 -0600

    deploy attempt

--- backend/server.js
+++ backend/server.js
@@ -16,12 +16,25 @@ const app = express();
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/projects', projectRoutes);
+
+const __dirname = path.resolve(); 
+
+if (process.env.NODE_ENV === 'production') { 
+  // set static folder
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  // any route that is not api will be redirected to index.html
+  app.get('*', (req, res) => 
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+);
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+});
+}
+
 
 app.use(notFound);
 app.use(errorHandler);
